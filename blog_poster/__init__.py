809d7cfda48182bd34026a63339986640ed0c126
Aris Stepe <arisstepe@gmail.com>
2018-11-03 11:38:56 -0400

    removed unnecessary files

--- blog_poster/__init__.py
+++ blog_poster/__init__.py
@@ -1,6 +1,6 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 jinja_env = Environment(
-loader=PackageLoader('blog_poster.templater', 'templates'),
+loader=PackageLoader('blog_poster.templates', 'templates'),
 autoescape=select_autoescape(['html'])
 )
